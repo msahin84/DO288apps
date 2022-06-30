3722ffcdcadf61ac73d25af0b2d0ccae5b61fe6d
Student User <student@workstation.lab.example.com>
2022-06-30 08:33:02 -0400

    for later use

---
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -0,0 +1,26 @@
+package com.redhat.training.example.javaserverhost.rest;
+
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import java.net.InetAddress;
+
+@Path("/")
+public class ServerHostEndPoint {
+
+  @GET
+  @Produces("text/plain")
+  public Response doGet() {
+    String host = "";
+    try {
+      host = InetAddress.getLocalHost().getHostName();
+    }
+    catch (Exception e) {
+       e.printStackTrace();
+    }
+    String msg = "I am running on server Mustafa Sahin  Version 0.1 Beta \n";
+    return Response.ok(msg).build();
+  }
+}
+
